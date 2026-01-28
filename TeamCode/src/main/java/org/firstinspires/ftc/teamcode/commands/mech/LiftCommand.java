f65baa26629d90f0bb4b9f20d0e0faa1f312ae87
rlagustj1 <hyeonseogim047@gmail.com>
2026-01-28 15:54:02 +0900

    Lift추가

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/mech/LiftCommand.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.commands.mech;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.subsystems.LiftSubsystem;
+
+public class LiftCommand extends CommandBase {
+
+    private LiftSubsystem lift;
+
+    public LiftCommand(LiftSubsystem lift) {
+        this.lift = lift;
+    }
+    public LiftCommand(LiftSubsystem lift, double max, double min) {
+        this.lift = lift;
+        LiftSubsystem.MAX = max;
+        LiftSubsystem.MIN = min;
+    }
+
+    @Override
+    public void initialize() {
+        super.initialize();
+        lift.setPosition(LiftSubsystem.MAX);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        super.end(interrupted);
+        lift.setPosition(LiftSubsystem.MIN);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LiftSubsystem.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class LiftSubsystem extends SubsystemBase {
+    private final Servo servo;
+
+    // 위치 값들을 서브시스템 내부에서 관리하면 좋습니다.
+    public static double MAX = 0.5;
+    public static double MIN = 0.7;
+
+    public LiftSubsystem(HardwareMap hardwareMap, String name) {
+        servo = hardwareMap.get(Servo.class, name);
+        servo.setDirection(Servo.Direction.FORWARD);
+    }
+    public LiftSubsystem(HardwareMap hardwareMap, String name, double max, double min) {
+        servo = hardwareMap.get(Servo.class, name);
+        servo.setDirection(Servo.Direction.FORWARD);
+    }
+
+
+    public void setPosition(double position) {
+        servo.setPosition(position);
+    }
+
+    public double getPosition() {
+        return servo.getPosition();
+    }
+}
